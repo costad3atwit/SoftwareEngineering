91ad5c8f308d6aef44ab0aecab5bebc834647ec2
David Costa <144468957+costad3atwit@users.noreply.github.com>
2025-10-12 16:49:15 -0400

    Implement card.py skeleton

--- backend/cards/card.py
+++ backend/cards/card.py
@@ -1,1 +1,22 @@
+from abc import ABC #abc's are python's version of (AB)stract (C)lasses 
+
+class Card(ABC):
+    description = ""
+    big_img = ""
+    small_img = ""
+
+    def __init__(self):
+        pass
+
+    def get_desc(self):
+        return self.description
+    
+    def get_big_img(self):
+        return self.big_img
+    
+    def get_small_img(self):
+        return self.small_img
+    
+    
+
 
