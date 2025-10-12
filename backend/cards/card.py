082b32518c68e1b71b57148633410c24b3fadb76
David Costa <144468957+costad3atwit@users.noreply.github.com>
2025-10-12 17:28:46 -0400

    Update card.py

--- backend/cards/card.py
+++ backend/cards/card.py
@@ -1,11 +1,39 @@
-from abc import ABC #abc's are python's version of (AB)stract (C)lasses 
+from abc import ABC, abstractmethod #abc's are python's version of (AB)stract (C)lasses 
+from enum import Enum
+
+class TargetType(Enum):
+    BOARD = "BOARD"
+    PIECE = "PIECE"
+    TIMER = "TIMER"
+    TURN = "TURN"
+
+class CardType(Enum):
+    UNSTABLE = "UNSTABLE"
+    CURSE = "CURSE"
+    HIDDEN = "HIDDEN"
+    TRANSFORM = "TRANSFORM"
+    FORCED = "FORCED"
+
 
 class Card(ABC):
+    id = ""
+    name = ""
     description = ""
     big_img = ""
     small_img = ""
 
-    def __init__(self):
+    def __init__(self, id: str, name: str, description: str, big_img: str, small_img: str):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.big_img = big_img
+        self.small_img = small_img  
+
+    @property
+    @abstractmethod
+    def card_type(self) -> CardType:
+        """Must be implemented in subclasses.
+        e.g. return CardType.CURSE"""
         pass
 
     def get_desc(self):
@@ -17,6 +45,11 @@ class Card(ABC):
     def get_small_img(self):
         return self.small_img
     
+    def get_id(self):
+        return self.id
     
-
+    def get_name(self):
+        return self.name
+    
+    
 
