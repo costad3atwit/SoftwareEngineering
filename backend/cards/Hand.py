6614b39bd77ad5182c36042df2bd40a2315efe8f
folajimiiatwit <144471241+folajimiiatwit@users.noreply.github.com>
2025-10-20 09:12:02 -0400

    hand

---
+++ backend/cards/Hand.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Hand:
+    def __init__(self):
+        self.cards: List[Card] = []
+
+    def add(self, card: Card) -> None:
+        self.cards.append(card)
+
+    def remove(self, card: Card) -> None:
+        if card in self.cards:
+            self.cards.remove(card)
+
+    def list(self) -> List[Card]:
+        return self.cards
+
+    def __len__(self):
+        return len(self.cards)
