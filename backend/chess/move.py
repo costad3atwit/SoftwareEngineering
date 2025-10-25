fd4e1756bb37035649cdd9727e4c9f5029571eb6
David Costa <144468957+costad3atwit@users.noreply.github.com>
2025-10-25 18:53:58 -0400

    Minor tweaks to move.py

--- backend/chess/move.py
+++ backend/chess/move.py
@@ -1,18 +1,18 @@
 from coordinate import Coordinate
 from piece import Piece
 class Move:
-    def __init__(self, from_sq: Coordinate, to_sq: Coordinate, piece: Piece,promotion=None, card_play_id=None, metadata=None):
+    def __init__(self, from_sq: Coordinate, to_sq: Coordinate, piece: Piece,promotion: Piece =None, card_play_id=None, metadata=None):
         self.from_sq = from_sq
         self.to_sq = to_sq
         self.piece = piece
-        self.promotion = promotion
+        self.promotion = promotion if promotion else None
         self.card_play_id = card_play_id
         self.metadata = metadata or {}
 
     def __str__(self):
         # Simple text version of the move 
         return f"{self.piece.algebraic_notation()}{self.from_sq}->{self.to_sq}" + (
-            f"={self.promotion}" if self.promotion else ""
+            f"={self.promotion.algebraic_notation}" if self.promotion else ""
         )
 
     def to_dict(self):
